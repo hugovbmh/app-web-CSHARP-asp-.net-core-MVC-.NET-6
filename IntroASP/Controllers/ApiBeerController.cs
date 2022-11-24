f9096e1873e43611d694667b59bde57f5964f5e3
Hugo <hugovilchezbeltran@gmail.com>
2022-11-24 11:36:49 -0500

    APP WEB

---
+++ IntroASP/Controllers/ApiBeerController.cs
@@ -0,0 +1,31 @@
+﻿using IntroASP.Models;
+using IntroASP.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //hereda de ControllerBase y retornará automaticamente un json
+    public class ApiBeerController : ControllerBase
+    {
+        private readonly PubContext _context;
+        public ApiBeerController(PubContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<BeerBrandViewModel>> Get() 
+        {
+            return await _context.Beers.Include(b=>b.Brand)
+                .Select(b=>new BeerBrandViewModel
+                {
+                    Name = b.Name,
+                    Brand = b.Brand.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ IntroASP/Controllers/BrandController.cs
@@ -0,0 +1,22 @@
+﻿using IntroASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    public class BrandController : Controller
+    {
+        // _ para campo privado
+        private readonly PubContext _context;
+        //controlador
+        public BrandController(PubContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Brands.ToListAsync());
+        }
+    }
+}
